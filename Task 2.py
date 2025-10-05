5e2eb7dce63d0d34f1290415fc489b2b6f39948e
Kumar Nitin <nitin12073@gmail.com>
2025-10-05 21:58:58 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number: "))
+import math
+sqrt = math.sqrt(n)
+natural_log = math.log(n)
+sine = math.sin(n)
+print("Square root : ",sqrt)
+print("Logarithm : ", natural_log)
+print("Sine :", sine)
